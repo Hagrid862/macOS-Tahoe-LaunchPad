5f31a825e4134a4511c3a51db72ad2e351c4c2a6
Nikodem Okroj <n.okroj@icloud.com>
2025-08-24 11:44:01 +0200

    Initial Commit

---
+++ tahoe-app-library/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tahoe-app-library
+//
+//  Created by Nikodem Okroj on 24/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tahoe-app-library/tahoe_app_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  tahoe_app_libraryApp.swift
+//  tahoe-app-library
+//
+//  Created by Nikodem Okroj on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct tahoe_app_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
